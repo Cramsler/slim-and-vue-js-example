46b8b5b1f121f0a32490113c3b50784614b92ab0
Glazunov Aleksandr <glazunov@elma-bpm.com>
2022-07-07 18:25:23 +0400

    init project

---
+++ config/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Factory\ContainerFactory;
+use Slim\App;
+
+require_once __DIR__ . '/../src/Support/Carbon.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Build DI Container instance
+$container = (new ContainerFactory())->createInstance();
+
+// Create App instance
+return $container->get(App::class);
---
+++ src/Support/Carbon.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Carbon;
+
+/**
+ * Carbon compat to speed up the composer autoloader.
+ *
+ * See: https://github.com/cakephp/chronos/issues/315
+ */
+final class Carbon
+{
+}
